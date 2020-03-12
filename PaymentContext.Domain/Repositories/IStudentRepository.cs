c18b2aa1688eac72937da9914884b872d7421bd4
Antonio Sucre <ajs2104@gmail.com>
2020-03-12 04:55:04 -0300

    Repository Pattern2

--- PaymentContext.Domain/Repositories/IStudentRepository.cs
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -1,7 +1,14 @@
+using PaymentContext.Domain.Entities;
+
 namespace PaymentContext.Domain.Repositories
 {
     public interface IStudentRepository
     {
         bool DocumentExist (string document);
+        bool EmailExist (string email);
+
+        void CreateSubscription(Student student);
+        
+
     }
 }
